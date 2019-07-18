b3f2563f02f8111d1649576de776a32e2ab0822b
Bruno Farias <bruno43ist@yahoo.com.br>
2019-07-18 12:47:43 -0300

    FIM AULA HERANCA

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import entities.BusinessAccount;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		BusinessAccount busacc = new BusinessAccount();
+		
+	}
+
+}
